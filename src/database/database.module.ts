0569bee189dc300c3e3333ec35c5b534edc2643d
jongminshin <ace4463@gmail.com>
2022-08-24 17:42:02 +0900

    Add: mysql-database.providers.spec

--- src/database/database.module.ts
+++ src/database/database.module.ts
@@ -1,4 +1,10 @@
 import { Module } from '@nestjs/common';
+import { mysqlDatabaseProviders } from './mysql-database.providers';
+import { ConfigModule } from '@nestjs/config';
 
-@Module({})
+@Module({
+  imports: [ConfigModule.forRoot()],
+  providers: [...mysqlDatabaseProviders],
+  exports: [...mysqlDatabaseProviders],
+})
 export class DatabaseModule {}
--- src/database/mysql-database.providers.spec.ts
+++ src/database/mysql-database.providers.spec.ts
@@ -1,13 +1,22 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { mysqlDatabaseProviders } from './mysql-database.providers';
+import { ConfigModule } from '@nestjs/config';
+import { Connection } from 'mysql2';
+import { DATA_SOURCE } from './database.constants';
 
 describe('MysqlDatabaseProviders', () => {
   let conn: any;
-  let dataSource: any;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
+      imports: [ConfigModule.forRoot()],
       providers: [...mysqlDatabaseProviders],
     }).compile();
+
+    conn = module.get<Connection>(DATA_SOURCE);
+  });
+
+  it('DATA_SOURCE should be defined', () => {
+    expect(conn).toBeDefined();
   });
 });
