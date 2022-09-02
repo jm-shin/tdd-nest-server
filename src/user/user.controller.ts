66dd0122aef583ad0fa408b00fcfe0bcd97c8531
jongminshin <ace4463@gmail.com>
2022-09-02 20:46:10 +0900

    Add: getUser

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,4 +1,20 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get, Param } from '@nestjs/common';
+import { UserService } from './user.service';
+import { UserEntity } from '../database/user.entity';
+import { from, Observable } from 'rxjs';
+import { map } from 'rxjs/operators';
 
 @Controller('user')
-export class UserController {}
+export class UserController {
+  constructor(private userService: UserService) {}
+
+  @Get(':id')
+  getUser(@Param('id') id: string): Observable<Partial<UserEntity>> {
+    return from(this.userService.findById(id)).pipe(
+      map((user) => {
+        delete user.password;
+        return user;
+      }),
+    );
+  }
+}
